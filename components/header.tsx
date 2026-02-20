51b24aaba0d2b3e860af847ea511dac5b58fac21
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2026-02-20 15:33:25 +0530

    Fix

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,20 @@
 import Logo from "./logo"
 import { ThemeSwitch } from "./theme.switch"
+import { Button } from "./ui/button"
+import { ChevronDown } from "lucide-react"
 
 export const Header = () => {
     return (
         <header className=" bg-background border-b">
-            <div className=" flex justify-between items-center gap-4 px-4 py-2">
+            <div className=" flex justify-between items-center gap-4 px-4 py-2.5">
                 <Logo />
-                <ThemeSwitch />
+                <div className=" flex items-center gap-2">
+                    <Button variant="ghost" >
+                        Tools
+                        <ChevronDown className=" size-4" />
+                    </Button>
+                    <ThemeSwitch />
+                </div>
             </div>
         </header>
     )
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,17 +1,10 @@
-import { Palette } from 'lucide-react';
-
+import Link from "next/link"
 const Logo = () => {
     return (
-        <div className=" flex items-center  gap-1.5">
-            <div className=" size-10 bg-linear-to-r from-primary to-chart-2 text-white flex-none rounded-full flex items-center justify-center">
-                <Palette className=" size-6" />
-            </div>
-            <span className=' text-muted-foreground/30'>|</span>
-            <p className=" text-xl font-extrabold tracking-wide leading-none ">
-                OrbColors
-            </p>
+        <Link href="/" className=" flex items-center gap-1  text-xl rounded-sm font-bold tracking-wide leading-none px-2.5 py-1.5 bg-linear-to-r from-primary to-chart-2 text-white ">
+            OrbColors
+        </Link>
 
-        </div>
     )
 }
 

59112e582a04e9cb23d5ca99cfdf9c025efe9ebd
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2026-02-20 17:03:35 +0530

    Fix

---
+++ app/_data/tools.ts
@@ -0,0 +1,42 @@
+export const tools = [
+    {
+        label: "Pallet Generator",
+        path: "/pallet-generator",
+        keyColor: "#2979FF",
+    },
+    {
+        label: "Explore Pallets",
+        path: "/explore-pallets",
+        keyColor: "#00E5A8",
+    },
+    {
+        label: "Tints and Shades",
+        path: "/tints-shades",
+        keyColor: "#FFC400",
+    },
+    {
+        label: "Colors List",
+        path: "/colors-list",
+        keyColor: "#FF1744",
+    },
+    {
+        label: "Border Radius Generate",
+        path: "/border-radius-generate",
+        keyColor: "#00B0FF",
+    },
+    {
+        label: "Text Formatter",
+        path: "/text-formatter",
+        keyColor: "#00E676",
+    },
+    {
+        label: "Html Editor",
+        path: "/html-editor",
+        keyColor: "#FF6D00",
+    },
+    {
+        label: "Contrast Checker",
+        path: "/contrast-checker",
+        keyColor: "#AA00FF",
+    },
+];

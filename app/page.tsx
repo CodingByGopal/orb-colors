4bf8b100e285af56cc3181cf08ba5c502547ca6c
Gopal Ji <105803289+CodingByGopal@users.noreply.github.com>
2026-02-20 22:07:59 +0530

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
+"use client"
+import { getRandomHexColor } from "@/app/_data/color-methods/orb.colors";
 export default function Page() {
+    console.log(getRandomHexColor())
     return <div>Hello World
-
     </div>;
 }
